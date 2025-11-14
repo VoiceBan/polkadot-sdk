1219c35a2d429601959093cb898e8436f71dc7c5
github-action-benchmark <github@users.noreply.github.com>
2025-11-14 16:42:19 +0000

    ___Updated docs

---
+++ master/trait.impl/pallet_assets/pallet/trait.BenchmarkHelper.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["assets_common",[["impl BenchmarkHelper&lt;Location, <a class=\"struct\" href=\"assets_common/local_and_foreign_assets/struct.ForeignAssetReserveData.html\" title=\"struct assets_common::local_and_foreign_assets::ForeignAssetReserveData\">ForeignAssetReserveData</a>&gt; for <a class=\"struct\" href=\"assets_common/benchmarks/struct.LocationAssetsBenchmarkHelper.html\" title=\"struct assets_common::benchmarks::LocationAssetsBenchmarkHelper\">LocationAssetsBenchmarkHelper</a>"]]],["pallet_assets",[]],["yet_another_parachain_runtime",[]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[483,21,37]}
